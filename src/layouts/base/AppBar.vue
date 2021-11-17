e1c295e4fd977205249be5952e7a59e1732b1f2f
jiahong96 <jiahong9705@gmail.com>
2021-11-17 23:53:43 +0800

    feat(app-bar): responsive layout

--- src/layouts/base/AppBar.vue
+++ src/layouts/base/AppBar.vue
@@ -1,11 +1,42 @@
 <template>
-  <nav class="navbar navbar-dark">
-    <div class="container-fluid mx-5">
-      <span class="navbar-brand mb-0 h1"><strong>Bower</strong>Bird</span>
+  <nav class="navbar navbar-expand-md navbar-dark bg-primary">
+    <div class="container-fluid mx-2">
+      <a
+        class="navbar-brand mb-0 h1"
+        href="/"
+      >Bower<span class="fw-light">Bird</span></a>
 
-      <div class="d-flex">
-        <span class="navbar-text">Images</span>
-        <span class="navbar-text">Pictures</span>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon" />
+      </button>
+
+      <div
+        id="navbarSupportedContent"
+        class="collapse navbar-collapse"
+      >
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a
+              class="nav-link text-white"
+              aria-current="page"
+              href="/"
+            >Images</a>
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link text-white"
+              href="/about"
+            >Pictures</a>
+          </li>
+        </ul>
       </div>
     </div>
   </nav>
@@ -14,9 +45,3 @@
 <script>
 export default { name: 'AppBar'}
 </script>
-
-<style scoped>
-.navbar {
-  background-color: #333333
-}
-</style>
