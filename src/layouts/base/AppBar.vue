70216009b1f8a2bc5b2f0c88952ddd12fcb294e2
Mike <jiahong9705@gmail.com>
2021-11-17 17:45:45 +0800

    feat: create base layout

---
+++ src/layouts/base/AppBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="navbar navbar-light bg-light">
+    <div class="container-fluid mx-5">
+      <span class="navbar-brand mb-0 h1"><strong>Bower</strong>Bird</span>
+
+      <div class="d-flex">
+        <span>Images</span>
+        <span>Pictures</span>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default { name: 'AppBar'}
+</script>
---
+++ src/layouts/base/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <app-bar />
+    <base-view />
+  </div>
+</template>
+
+<script>
+export default {
+  components: { 
+    AppBar: () => import('./AppBar.vue'), 
+    BaseView: () => import('./View.vue'), 
+  },
+}
+</script>
