37ae2bb7ffc008495e415a26c6cc821f333fdacc
jiahong96 <jiahong9705@gmail.com>
2021-11-20 17:06:12 +0800

    feat(router): add global guard for auth

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from '../store'
 import routes from './routes'
 
 Vue.use(VueRouter)
@@ -8,6 +9,16 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+const isAuthenticated = !!store.getters['auth/authorized']
+
+router.beforeEach((to, from, next) => {
+  if (to.name !== 'Login' && !isAuthenticated) next({ name: 'Login' })
+
+  else if (to.name === 'Login' && isAuthenticated) next({ name: 'Home' })
+
+  else next()
 })
 
 export default router
