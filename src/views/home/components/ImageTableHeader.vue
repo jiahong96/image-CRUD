e00867286ca14600c7ffe5f4f516fda3a2c50798
jiahong96 <jiahong9705@gmail.com>
2021-11-21 22:51:08 +0800

    feat: render tag filter group

--- src/views/home/components/ImageTableHeader.vue
+++ src/views/home/components/ImageTableHeader.vue
@@ -1,8 +1,33 @@
 <template>
   <div class="card-header bg-white p-5 border-bottom-0">
-    <h5 class="card-title">
+    <h4 class="card-title mb-4">
       Images
-    </h5>
+    </h4>
+    <div
+      class="btn-group row gx-0 gy-2"
+      role="group"
+      aria-label="Filter group"
+    >
+      <div
+        v-for="tag in tags"
+        :key="tag.id"
+        class="col-auto"
+      >
+        <input
+          :id="tag.id"
+          name="btnradio"
+          type="radio"
+          class="btn-check"
+          autocomplete="off"
+          :checked="tag.id === 1"
+        >
+        <label
+          class="btn btn-sm btn-outline-primary me-2 rounded-pill"
+          :for="tag.id"
+          @click="filter(tag)"
+        > {{ `${tag.name} (${tag.images_count})` }} </label>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -34,4 +59,9 @@ export default {
     totalImageCount (tags) {
       return tags.reduce((total, tag) => total += tag.images_count, 0) 
     },
+    filter (tag) {
+      this.$emit('filter', tag)
+    }
+  },
+}
 </script>
