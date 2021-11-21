b21d1405fc90a2b70828accfb044cc16e64d6c27
jiahong96 <jiahong9705@gmail.com>
2021-11-21 16:37:50 +0800

    feat: create image tag component

---
+++ src/components/ImageTag.vue
@@ -0,0 +1,14 @@
+<template>
+  <span class="badge bg-light text-dark mx-0.5">{{ text }}</span>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+  },
+}
+</script>
