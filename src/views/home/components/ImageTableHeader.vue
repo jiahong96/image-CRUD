a142343395785c7b9ede2ef69299ed7f4683a3e9
jiahong96 <jiahong9705@gmail.com>
2021-11-21 22:49:41 +0800

    feat: get tag list

--- src/views/home/components/ImageTableHeader.vue
+++ src/views/home/components/ImageTableHeader.vue
@@ -7,5 +7,31 @@
 </template>
 
 <script>
-export default {}
+import TagService from '@/api/TagService'
+
+export default {
+  data() {
+    return {
+      tags: []
+    }
+  },
+  created () {
+    this.getTags()
+  },
+  methods: {
+    async getTags () {
+      try {
+        const response = await TagService.list()
+
+        this.tags = [
+          ...[ { id: 1, name: 'All', images_count: this.totalImageCount(response.data) } ], 
+          ...response.data
+        ]
+      } catch (error) {
+        console.log(error)
+      }    
+    },
+    totalImageCount (tags) {
+      return tags.reduce((total, tag) => total += tag.images_count, 0) 
+    },
 </script>
