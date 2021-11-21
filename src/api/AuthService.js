7073fa62bc06f198df4a2a623e5c781e2d79e443
jiahong96 <jiahong9705@gmail.com>
2021-11-21 16:37:03 +0800

    refactor(env): exclude /api from base url

--- src/api/AuthService.js
+++ src/api/AuthService.js
@@ -2,6 +2,6 @@ import Vue from 'vue'
 
 export default {
   login({ username, password }) {
-    return Vue.prototype.$http.post(`/v1/login`, { user: { email:username, password } })
+    return Vue.prototype.$http.post(`/api/v1/login`, { user: { email:username, password } })
   }
 }
