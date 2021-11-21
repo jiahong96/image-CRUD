e9e02812ccceb8aab8c9f0c7d25feb9b7fb59d7d
jiahong96 <jiahong9705@gmail.com>
2021-11-21 20:50:39 +0800

    feat(image): create header component

---
+++ src/views/home/components/ImageTableHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="card-header bg-white p-5 border-bottom-0">
+    <h5 class="card-title">
+      Images
+    </h5>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
