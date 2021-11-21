43f50c7b625733095b43dc1befbf307937366723
jiahong96 <jiahong9705@gmail.com>
2021-11-21 21:03:40 +0800

    feat(home): import image table

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="home">
-    <h1> This is home page </h1>
+  <div class="container-fluid py-5 px-3 p-md-5">
+    <image-table />
   </div>
 </template>
 
 <script>
+import ImageTable from './components/ImageTable.vue'
+
 export default {
   name: 'Home',
+  components: { ImageTable },
 }
 </script>
