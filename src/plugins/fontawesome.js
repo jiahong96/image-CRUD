27d7500a8903bdca439e5e811042a3882d164eea
jiahong96 <jiahong9705@gmail.com>
2021-11-21 23:19:41 +0800

    feat(icon): import search icon

--- src/plugins/fontawesome.js
+++ src/plugins/fontawesome.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faImage, faHashtag } from '@fortawesome/free-solid-svg-icons'
+import { faImage, faHashtag, faSearch } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
 
 // select and import specific icons that you need
 // so that the final bundled file only includes the selected icons
-library.add(faImage, faHashtag)
+library.add(faImage, faHashtag, faSearch)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
