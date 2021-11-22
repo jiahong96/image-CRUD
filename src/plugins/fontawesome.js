561c7f8f8da79b74c72e405471161236b545cf0d
jiahong96 <jiahong9705@gmail.com>
2021-11-22 22:28:51 +0800

    chore(icon): add chevrons

--- src/plugins/fontawesome.js
+++ src/plugins/fontawesome.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faImage, faHashtag, faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faImage, faHashtag, faSearch, faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
 
 // select and import specific icons that you need
 // so that the final bundled file only includes the selected icons
-library.add(faImage, faHashtag, faSearch)
+library.add(faImage, faHashtag, faSearch, faChevronDown, faChevronUp)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
