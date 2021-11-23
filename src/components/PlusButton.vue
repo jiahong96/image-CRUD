faa0866a833af73739159d4f0e70c23a43cb5065
Mike <jiahong9705@gmail.com>
2021-11-23 15:51:43 +0800

    feat(component): create rounded plus button

---
+++ src/components/PlusButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <button
+    class="btn btn-sm btn-light rounded-circle"
+    @click="$emit('click')"
+  >
+    <font-awesome-icon :icon="['fas', 'plus']" />
+  </button>
+</template>
+
+<style scoped>
+button.btn.btn-light { 
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.1);
+}
+</style>
