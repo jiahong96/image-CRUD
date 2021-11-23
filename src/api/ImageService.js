3b60216968051b64a113be6c51ed4fc75ed6037a
Mike <jiahong9705@gmail.com>
2021-11-23 11:20:37 +0800

    feat(api): add CREATE api for image

--- src/api/ImageService.js
+++ src/api/ImageService.js
@@ -5,5 +5,15 @@ const resource = 'images'
 export default {
   list (q = '', tagged = '') {
     return Vue.prototype.$http.get(`/api/v1/${resource}?q=${q}&tagged=${tagged}`)
+  },
+  create (data) {
+    return Vue.prototype.$http.request({
+      url: `/api/v1/${resource}`,
+      method: 'post',
+      data,
+      config: {
+        headers: { 'Content-Type': 'multipart/form-data' },
+      },
+    })
   }
 }
