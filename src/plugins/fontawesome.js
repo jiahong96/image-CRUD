f345a36ba84ec63170ac94414d3da9694fa43549
Mike <jiahong9705@gmail.com>
2021-11-23 11:18:01 +0800

    feat(icon): import plus icon

--- src/plugins/fontawesome.js
+++ src/plugins/fontawesome.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faImage, faHashtag, faSearch, faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
+import { faImage, faHashtag, faSearch, faChevronDown, faChevronUp, faPlus } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
 
 // select and import specific icons that you need
 // so that the final bundled file only includes the selected icons
-library.add(faImage, faHashtag, faSearch, faChevronDown, faChevronUp)
+library.add(faImage, faHashtag, faSearch, faChevronDown, faChevronUp, faPlus)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
