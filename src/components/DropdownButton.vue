d398ba7566f771e523a8b487cae762049972e782
Mike <jiahong9705@gmail.com>
2021-11-24 11:05:44 +0800

    feat(component): create reusable dropdown button

---
+++ src/components/DropdownButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <button
+      class="btn btn-light dropdown-toggle dropdown-el"
+      type="button"
+      data-bs-toggle="dropdown"
+    >
+      <span class="me-2">{{ title }}</span>
+      <font-awesome-icon :icon="['fas', 'ellipsis-v']" />
+    </button>
+    <ul
+      class="dropdown-menu rounded-3 dropdown-el"
+    >
+      <slot name="item" />
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true 
+    },
+  },
+}
+</script>
+
+<style scoped>
+.dropdown-el {
+  min-width: 110px
+}
+
+.dropdown-toggle::after {
+  display: none;
+}
+</style>
