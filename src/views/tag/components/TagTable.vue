1fb5277221a21865a1a7403b221cd7ef6bbff1b7
jiahong96 <jiahong9705@gmail.com>
2021-11-24 20:22:30 +0800

    feat: create tag table

---
+++ src/views/tag/components/TagTable.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="card card-shadow">
+    <!-- header -->
+    <!-- body -->
+    <!-- footer -->
+  </div>
+</template>
+
+<script>
+import TagService from '@/api/TagService'
+
+export default {
+  data() {
+    return {
+      tags: null,
+      isLoading: false
+    }
+  },
+  created () {
+    this.getTags()
+  },
+  methods: {
+    async getTags () {
+      this.isLoading = true
+      try {
+        const response = await TagService.list()
+        this.tags = response.data
+      } catch (error) {
+        console.log(error)
+      }    
+      
+      this.isLoading = false
+    }, 
+  },
+}
+</script>
--- src/views/tag/index.vue
+++ src/views/tag/index.vue
@@ -1,11 +1,13 @@
 <template>
-  <div>
-    <h1>Tag</h1>
+  <div class="container-fluid py-5 px-3 p-md-5">
+    <tag-table />
   </div>
 </template>
 
 <script>
+import TagTable from './components/TagTable.vue'
 export default {
-    
+  name: 'TagIndex',
+  components: { TagTable },
 }
 </script>
