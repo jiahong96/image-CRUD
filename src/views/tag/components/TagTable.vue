1ccdce12a89b00948a88358ae70cb4f0ac9d3c32
jiahong96 <jiahong9705@gmail.com>
2021-11-24 20:53:53 +0800

    feat(tag-table): add headers

--- src/views/tag/components/TagTable.vue
+++ src/views/tag/components/TagTable.vue
@@ -1,7 +1,26 @@
 <template>
   <div class="card card-shadow">
     <tag-table-header @search="setSearch" />
-    <!-- body -->
+    
+    <div
+      v-show="!isLoading"
+      class="card-body table-responsive p-0"
+    >
+      <table class="table">
+        <thead>
+          <tr>
+            <th
+              v-for="(header, i) in headers"
+              :key="i"
+              :class="header.class"
+              scope="col"
+            >
+              {{ header.name }}
+            </th>
+          </tr>
+        </thead>
+      </table>
+    </div>
     <!-- footer -->
   </div>
 </template>
@@ -16,7 +35,12 @@ export default {
     return {
       tags: null,
       isLoading: false,
-      search: null
+      search: null,
+      headers: [
+        {value: 'tag', name: 'Tag', class: 'ps-5'},
+        {value: 'total', name: 'Total Images', class: 'text-center fw-normal'},
+        {value: 'action', name: '', class: 'text-center'}
+      ]
     }
   },
   created () {
